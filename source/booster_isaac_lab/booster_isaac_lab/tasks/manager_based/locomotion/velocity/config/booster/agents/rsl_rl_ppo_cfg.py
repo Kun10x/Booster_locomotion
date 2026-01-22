7fb996bddb3ee8a7b9e7ea210620a85c9e0fda4b
Kun10x <79007897+Kun10x@users.noreply.github.com>
2026-01-22 20:48:09 +0000

    Adjust PPO configuration parameters for training

--- source/booster_isaac_lab/booster_isaac_lab/tasks/manager_based/locomotion/velocity/config/booster/agents/rsl_rl_ppo_cfg.py
+++ source/booster_isaac_lab/booster_isaac_lab/tasks/manager_based/locomotion/velocity/config/booster/agents/rsl_rl_ppo_cfg.py
@@ -4,8 +4,8 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 @configclass
 class BoosterRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
-    max_iterations = 30000
-    save_interval = 200
+    max_iterations = 10000
+    save_interval = 500
     experiment_name = "booster_rough"
     empirical_normalization = False
     
@@ -20,12 +20,12 @@ class BoosterRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         value_loss_coef = 1.0,
         use_clipped_value_loss = True,
         clip_param = 0.2,
-        entropy_coef = 0.01,
+        entropy_coef = -0.01,
         num_learning_epochs = 5,
         num_mini_batches = 4,
-        learning_rate = 1.0e-3,
+        learning_rate = 1.0e-5,
         schedule = "adaptive",
-        gamma = 0.99,
+        gamma = 0.995,
         lam = 0.95,
         desired_kl = 0.01,
         max_grad_norm = 1.0,
